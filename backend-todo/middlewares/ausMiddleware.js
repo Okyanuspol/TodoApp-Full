5a7f7519d2937eed157bc0f3115fc19c35bcc20e
suekrue <suekrue.cebeci@dci-student.org>
2024-10-08 13:36:46 +0200

    ignore

--- backend-todo/middlewares/ausMiddleware.js
+++
@@ -1,8 +0,0 @@
-export function authMiddleware(req,res, next) {
-    const isLoggedIn = req.headers.authorization;
-    if (isLoggedIn) {
-      next()
-    } else {
-      res.status(401).send("Sorry");
-    }
-  }
