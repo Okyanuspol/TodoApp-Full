3c33ddf30a88a70c039b08413ba305309b410401
suekrue <suekrue.cebeci@dci-student.org>
2024-10-18 10:27:24 +0200

    neu

---
+++ backend-todo/db.js
@@ -0,0 +1,20 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.ATLAS_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export async function connectDB() {
+    if (!client.topology || !client.topology.isConnected()) {
+        await client.connect();
+      }
+  const db = client.db("todoDB");
+  return db;
+}
--- backend-todo/server.js
+++ backend-todo/server.js
@@ -1,39 +1,93 @@
 import express from 'express';
 import cors from 'cors';
+import { ObjectId } from 'mongodb';
+import { connectDB } from './db.js';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-let todos = [];
-
-app.get('/todos', (req, res) => {
-  res.json(todos);
-});
-
-app.post('/todos', (req, res) => {
-  const newTodo = { id: Date.now(), title: req.body.title, completed: false };
-  todos.push(newTodo);
-  res.json(newTodo);
-});
-
-app.delete('/todos/:id', (req, res) => {
-  const todoId = parseInt(req.params.id);
-  todos = todos.filter((todo) => todo.id !== todoId);
-  res.status(200).send("Todo gelöcht");
-});
-
-app.patch('/todos/:id', (req, res) => {
-  const todoId = parseInt(req.params.id);
-  const todo = todos.find((todo) => todo.id === todoId);
-  if (todo) {
-    todo.completed = !todo.completed;
-    res.json(todo);
-  } else {
-    res.status(404).json({ error: 'Todo nicht gefunden' });
+app.get("/", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+    const data = await todosCollection.find({}).toArray();
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
+
+app.get("/test", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+    const data = await todosCollection.findOne({name: "Aufräumen"});
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+    const data = await todosCollection.findOne({_id: new ObjectId(req.params.id)});
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/", async (req, res) => {
+  const newTodo = req.body;
+  
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+
+    const data = await todosCollection.insertOne(newTodo);
+    res.json({message: "insertion successful", insertedId: data.insertedId});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+      const database = await connectDB();
+      const todosCollection = database.collection("todos");
+
+      const data = await todosCollection.deleteOne({_id: new ObjectId(id)});
+      res.json(data);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+})
+
+app.put("/:id", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+
+    const updatedTodo = req.body;
+    const data = await todosCollection.updateOne({_id: new ObjectId(req.params.id)}, {$set: updatedTodo})
+    
+    if (data.matchedCount === 1) {
+      res.json({message: "insertion successful", data: data});
+    } else {
+      res.status(404).json({message: "Todo not found"})
+    }
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
 
 app.use((err, req, res, next) => {
     console.error("error")
@@ -44,6 +98,4 @@ app.use ((req, res) => {
     res.status(404).send("Die Seite, die du suchst, existiert nicht ");
 })
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000}`);
-});
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
